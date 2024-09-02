6612b4210f08247d200b5eda6f6c3807b63f9e28
Adriano0702 <adriano.silva.nery@hotmail.com>
2024-09-02 06:21:31 -0300

    falta corrigir porcentagem

--- WF_Projeto_Calculos/WF_Projeto_Calculos/frmCalcSuper.cs
+++ WF_Projeto_Calculos/WF_Projeto_Calculos/frmCalcSuper.cs
@@ -23,17 +23,17 @@ namespace WF_Projeto_Calculos
 
         public void f_Numeros (object sender, EventArgs e)
         {
-            
+            // Se o visor estiver em zero eo botão acionado não for virgula, limpar ele para adicionar um novo número
             if (lblvisor.Text == "0" && ((Button)sender).Text != ",")
                 
                 lblvisor.Text = "";
 
+            /* Se o botão não for uma virgula ou se o botão for uma virgula e o visor conter uma vigula 
+             adicione o texto do botão*/
             if (((Button)sender).Text != "," || 
-               (((Button)sender).Text == "," &&  // "||/ou" "!="diferente" "&&/e"  "!/NAO"
+               (((Button)sender).Text == "," &&           // "||/ou" "!="diferente" "&&/e"  "!/NAO"
                !lblvisor.Text.Contains(","))) 
-
             {
-
                 lblvisor.Text += ((Button)sender).Text;
             }
 
@@ -41,44 +41,62 @@ namespace WF_Projeto_Calculos
 
         public void f_Operacoes(object sender, EventArgs e)
         {
-            numAnt = decimal.Parse(lblvisor.Text);
+            //Pega o nunmero do visor e armazena na variavel numAnt
+            numAnt = decimal.Parse(lblvisor.Text); 
+            
+            //armazena operação selecionada
             vOperacao = ((Button)sender).Text;
+
+            //
             vLimpa = true;
-            lblHistorico.Text += numAnt + " " + vOperacao;
+
+            // Atuliza o historico com número atual e a operação selecionada
+            lblHistorico.Text += numAnt + " " + vOperacao+ "";
+
+            //limpa o visor para proxima entrada
+            lblvisor.Text = string.Empty;
+
+            //Foca o visor, preparando para proxima entrada do usuario
             lblvisor.Focus();
         }
 
-        private void btnIgual_Click(object sender, EventArgs e)
+        private void btnIgual_Click_1(object sender, EventArgs e)
         {
-            decimal f_Operacoes = decimal.Parse(lblvisor.Text);
+            // Obtém o numero atual do visor 
+            decimal numAtual = decimal.Parse(lblvisor.Text);
+            decimal resultado = 0 ;
 
+            // Calcula o resultado com base na operação armazenada
             switch (vOperacao)
             {
                 case "+":
                     {
-                        lblvisor.Text = (numAnt + numAnt).ToString();
-                        lblHistorico.Text += numAnt + "+";
+                        resultado = numAnt + numAtual;                                                  
                         break;
                     }
                 case "-":
                     {
-                        lblvisor.Text = (numAnt + numAnt).ToString();
-                        lblHistorico.Text += numAnt + "-";
+                        resultado = numAnt - numAtual;                        
                         break;
                     }
-                case "*":
+                case "x":
                     {
-                        lblvisor.Text = (numAnt + numAnt).ToString();
-                        lblHistorico.Text += numAnt + "*";
+                        resultado = numAnt * numAtual;                        
                         break;
                     }
                 case "/":
                     {
-                        lblvisor.Text = (numAnt + numAnt).ToString();
-                        lblHistorico.Text += numAnt + "M";
+                        resultado = numAnt / numAtual;                        
+                        break;                    
+                    }
+                case "%":
+                    {
+                        resultado = numAnt * (numAtual / 100);
+                        
                         break;
                     }
             }
+             lblvisor.Text = resultado.ToString();
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
@@ -96,9 +114,6 @@ namespace WF_Projeto_Calculos
         
         }
 
-        private void btnIgual_Click_1(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
